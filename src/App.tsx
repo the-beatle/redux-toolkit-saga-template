6b321a571f604065801b48874c3fff0616e8cf85
mario <marcae7@gmail.com>
2022-04-04 18:58:16 -0500

    subscriptions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Operators from "./features/Rxjs/Operators";
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Observables from "./features/Rxjs/Observables";
 import Observers from "./features/Rxjs/Observers";
+import Subscriptions from "./features/Rxjs/Subscriptions";
 
 function App() {
   return (
@@ -12,11 +13,14 @@ function App() {
           <Link to="/observables">Observable</Link>
           <Link to="/observers">Observers</Link>
           <Link to="/operators">Operators</Link>
+          <Link to="/subscriptions">Subscriptions</Link>
+
         </nav>
         <Switch>
         <Route exact path="/observables" component={Observables} />
         <Route exact path="/observers" component={Observers} />
         <Route exact path="/operators" component={Operators} />
+        <Route exact path="/subscriptions" component={Subscriptions} />
         </Switch>
       </div>
     </Router>
---
+++ src/features/Rxjs/Subscriptions.tsx
@@ -0,0 +1,41 @@
+import { fromEvent, interval, of } from 'rxjs'
+import { useEffect, useRef, useState } from "react";
+import { map,first, delay } from 'rxjs/operators';
+import { FromEventTarget } from 'rxjs/internal/observable/fromEvent'
+
+function Subscriptions() {
+  const [value,setValue] = useState(0)
+  
+  useEffect(() => {
+
+    console.log('subscriptions initial');
+
+    return () => console.log("subscriptions done");
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+        A Subscription essentially just has an unsubscribe() function to release resources or cancel Observable executions.
+      </p>
+      <p>Subscriptions can also be put together, so that a call to an unsubscribe() of one Subscription may unsubscribe multiple Subscriptions.</p>
+      </header>
+    </div>
+  );
+}
+
+export default Subscriptions;
+
+const observable1 = interval(400);
+const observable2 = interval(300);
+ 
+const subscription = observable1.subscribe(x => console.log('first: ' + x));
+const childSubscription = observable2.subscribe(x => console.log('second: ' + x));
+ 
+subscription.add(childSubscription);
+ 
+setTimeout(() => {
+  // Unsubscribes BOTH subscription and childSubscription
+  subscription.unsubscribe();
+}, 1000);
